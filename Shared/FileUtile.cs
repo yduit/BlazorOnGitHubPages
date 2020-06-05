78d378af6d02d013cf12530517408a201bd003b4
Yong Du <y.du@live.com>
2020-06-05 17:39:51 -0400

    Excel download added to FetchData page

---
+++ Shared/FileUtile.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace BlazorOnGitHubPages
+{
+    public static class FileUtil
+    {
+        public static ValueTask SaveAs(this IJSRuntime js, string filename, byte[] data)
+            => js.InvokeVoidAsync(
+                "saveAsFile",
+                filename,
+                Convert.ToBase64String(data));
+    }
+}
