f8b634f1b27c987231577bec3eb523c447ba5f03
Yong Du <y.du@live.com>
2020-06-07 23:03:23 -0400

    device monitor and ML.NET code added

---
+++ Shared/AppSettings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorOnGitHubPages.Shared
+{
+    public class AppSettings
+    {
+        public Connectionstrings ConnectionStrings { get; set; }
+        public Logging Logging { get; set; }
+        public string AllowedHosts { get; set; }
+        public Mlmodel MLModel { get; set; }
+        public string AlphaVantageApiKey { get; set; }
+        public string DeviceHubUrl { get; set; }
+    }
+
+    public class Connectionstrings
+    {
+        public string DefaultConnection { get; set; }
+        public string AppConnection { get; set; }
+    }
+
+    public class Logging
+    {
+        public Loglevel LogLevel { get; set; }
+    }
+
+    public class Loglevel
+    {
+        public string Default { get; set; }
+        public string Microsoft { get; set; }
+        public string MicrosoftHostingLifetime { get; set; }
+    }
+
+    public class Mlmodel
+    {
+        public string MLModelFilePath { get; set; }
+    }
+}
