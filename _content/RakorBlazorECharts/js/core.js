b6f02568578f1f1f08964f8352a82bd816f4b169
yduit <yduit@users.noreply.github.com>
2020-06-29 02:20:56 +0000

    Deploy to GitHub pages

---
+++ _content/RakorBlazorECharts/js/core.js
@@ -0,0 +1,94 @@
+﻿/// <reference path="jquery-3.4.1.js" />
+
+window.echartsFunctions = {
+    liChart : new Array(),
+    getChart: function (id) {
+        for (var i = 0; i < echartsFunctions.liChart.length; i++) {
+            if (echartsFunctions.liChart[i].Id === id) {
+                return echartsFunctions.liChart[i].Chart; 
+            }
+        }
+        return null;
+    },
+    addChart: function (id, chart) {
+        echartsFunctions.liChart.push({ Id: id, Chart: chart });
+    },
+    removeChart: function (id) {
+        var index = -1;
+        for (var i = 0; i < echartsFunctions.liChart.length; i++) {
+            if (echartsFunctions.liChart[i].Id === id) {
+                index = i;
+                break;
+            }
+        }
+        if (index >= 0) {
+            echartsFunctions.liChart.splice(index, 1);
+        }
+    },
+    initChart: function (id) {
+        var chart = echartsFunctions.getChart(id);
+        if (chart === null) {
+            //chart = echarts.init(document.getElementById(id), null, { renderer: 'svg' });
+            chart = echarts.init(document.getElementById(id));
+            chart.showLoading();
+            echartsFunctions.addChart(id, chart);
+        }
+        return chart;
+    },
+    showLoading: function (id) {
+        var chart = echartsFunctions.getChart(id);
+        if (chart === null) {
+            echartsFunctions.initChart(id);
+        }
+        else
+            chart.showLoading();
+    },
+    hideLoding: function (id) {
+        var chart = echartsFunctions.getChart(id);
+        if (chart !== null)
+            chart.hideLoading();
+    },
+    setupChart: function (id, option, notMerge) {
+        console.log('参数：');
+        console.log(option);
+        var opt = eval('(' + option + ')');
+        console.log(opt);
+        var chart = echartsFunctions.getChart(id);
+        if (chart === null) {
+            chart=echartsFunctions.initChart(id);
+        }
+        chart.hideLoading();
+        chart.setOption(opt, notMerge);
+        //echarts.init(document.getElementById(id)).setOption(opt);
+    },
+    execFunc: function (id, func, prefix) {
+        var chart = echartsFunctions.getChart(id);
+        if (chart === null) {
+            chart=echartsFunctions.initChart(id);
+        }
+        if (prefix)
+            eval('chart.' + func);
+        else
+            eval(func);
+        console.log('已执行：' + func);
+    },
+    execInvokeMethod: function (className,methodName) {
+        return DotNet.invokeMethodAsync(className,methodName);
+    },
+    registerMap: function (id,name, url,_callback) {
+        //var chart = echartsFunctions.getChart(id);
+        //if (chart === null) {
+        //    chart = echartsFunctions.initChart(id);
+        //}
+        //chart.hideLoading();
+        $.getJSON(url)
+            .done(function (data) {
+                echarts.registerMap(name, data);
+                if (_callback !== null)
+                    eval(_callback);
+            })
+            .fail(function (jqxhr, textStatus, error) {
+                console.log("获取json数据（" + url+"）失败: " + textStatus + "（" + error+"）");
+            });
+    }
+};
---
+++ saveAsFile.js
@@ -0,0 +1,18 @@
+﻿function saveAsFile(filename, bytesBase64) {
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = "data:application/octet-stream;base64," + bytesBase64;
+    document.body.appendChild(link); // Needed for Firefox
+    link.click();
+    document.body.removeChild(link);
+}
+
+function plot(id) {
+    var data = [{ "type": "bar", "x": [1.0, 2.0, 3.0, 4.0, 5.0], "y": [10.0, 20.0, 83.0, 34.0, 15.0] }];
+    var layout = "";
+    Plotly.newPlot(id, data, layout);
+}
+
+function execute(jscode) {
+    eval(jscode);
+}
