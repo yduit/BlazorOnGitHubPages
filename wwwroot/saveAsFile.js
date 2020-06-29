43dac9effa192f128a4a503baf0d184c3d29a1ef
Yong Du <y.du@live.com>
2020-06-28 21:33:20 -0400

    DataFrame

--- wwwroot/saveAsFile.js
+++ wwwroot/saveAsFile.js
@@ -6,3 +6,13 @@
     link.click();
     document.body.removeChild(link);
 }
+
+function plot(id) {
+    var data = [{ "type": "bar", "x": [1.0, 2.0, 3.0, 4.0, 5.0], "y": [10.0, 20.0, 83.0, 34.0, 15.0] }];
+    var layout = "";
+    Plotly.newPlot(id, data, layout);
+}
+
+function execute(jscode) {
+    eval(jscode);
+}
